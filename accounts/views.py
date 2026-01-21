483e42107790894a850d9f666c9f4454a8a03f9a
Sofa <sofo4kavakula@gmail.com>
2026-01-21 12:50:39 +0200

    Fixed success_url for sign up in accounts

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,7 @@ from .forms import SignUpForm, UserUpdateForm
 class SignUpView(generic.CreateView):
     """User registration view"""
     form_class = SignUpForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("accounts:login")
     template_name = "registration/signup.html"
 
     def form_valid(self, form):
